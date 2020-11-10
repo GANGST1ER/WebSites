f552b1a97a8958f7e470c3a46117898a17106e69
GANGST1ER <gangst22er@gmail.com>
2020-11-10 12:38:16 +0300

    slider-first

---
+++ site_03/src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        // adaptiveHeight: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right.svg"></button>'
+    });
+});
